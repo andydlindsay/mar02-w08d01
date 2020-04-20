95e83e452cceb3c07f9dfa4893c6c24174d47fcb
Andy Lindsay <andydlindsay@gmail.com>
2020-04-20 12:48:23 -0700

    finish adding tests

--- integration-testing/src/components/Result.jsx
+++ integration-testing/src/components/Result.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+import { genFeedbackMessage } from '../helpers/helpers';
 
-const Result = () => {
+const Result = (props) => {
   const [highScores, setHighScores] = React.useState([]);
+  const message = genFeedbackMessage(props.status);
 
   const fetchHighScores = () => {
     axios
@@ -13,7 +15,7 @@ const Result = () => {
 
   return(
     <footer data-testid="result_footer">
-      <h2>Waiting for your choice!</h2>
+      <h2>{message}</h2>
       <button onClick={fetchHighScores} data-testid="fetch-highscores">Fetch HighScores</button>
       { highScores.map(highScore => (<li key={highScore.id}>{highScore.name}</li>)) }
     </footer>
--- integration-testing/src/components/__tests__/Result.test.js
+++ integration-testing/src/components/__tests__/Result.test.js
@@ -14,45 +14,78 @@ const data = {
   ]
 };
 
-test('shows appropriate message when the status is "Waiting"', () => {
-  const fakeState = {
-    compSelection: null,
-    playerSelection: null,
-    status: 'Waiting',
-    cheating: false
-  };
-  
-  const { container } = render(<Result status={fakeState.status} />);
+describe('Displays appropriate messages', () => {
+  let fakeState;
 
-  expect(getByTestId(container, 'result_footer')).toHaveTextContent('Waiting for your choice!');
+  beforeEach(() => {
+    fakeState = {
+      compSelection: null,
+      playerSelection: null,
+      status: 'Waiting',
+      cheating: false
+    };
+  });
+
+  test('shows appropriate message when the status is "Waiting"', () => {
+    fakeState.status = 'Waiting';
+    const { container } = render(<Result status={fakeState.status} />);
+  
+    expect(getByTestId(container, 'result_footer')).toHaveTextContent('Waiting for your choice!');
+  });
+
+  test('shows appropriate message when the status is "Won"', () => {
+    fakeState.status = 'Won';
+    const { container } = render(<Result status={fakeState.status} />);
+  
+    expect(getByTestId(container, 'result_footer')).toHaveTextContent('Good job!');
+  });
+
+  test('shows appropriate message when the status is "Lost"', () => {
+    fakeState.status = 'Lost';
+    const { container } = render(<Result status={fakeState.status} />);
+  
+    expect(getByTestId(container, 'result_footer')).toHaveTextContent('You lost!');
+  });
+
+  test('shows appropriate message when the status is "Tied"', () => {
+    fakeState.status = 'Tied';
+    const { container } = render(<Result status={fakeState.status} />);
+  
+    expect(getByTestId(container, 'result_footer')).toHaveTextContent('Tie game!');
+  });
 });
 
-// this function must be marked as `async` in order to use `await` within it
-test('Axios test', async () => {
-  const { getByTestId, findByText } = render(<Result status="Waiting" />);
-  const button = getByTestId('fetch-highscores');
-
-  // mock any calls to axios.get with hardcoded return value `data`
-  axios.get.mockResolvedValueOnce({ data });
-
-  // firing this event causes the axios call to happen
-  fireEvent.click(button);
-
-  // findBy functions return a promise which we can `await`
-  await findByText('Bob');
-});
-
-test('functions', () => {
-  const mockFn = jest.fn();
-  let result = mockFn('hello');
-
-  expect(result).toBeUndefined();
-  expect(mockFn).toHaveBeenCalled();
-  expect(mockFn).toHaveBeenCalledWith('hello');
-  expect(mockFn).toHaveBeenCalledTimes(1);
-
-  const mockFn2 = jest.fn(() => 'world');
-  result = mockFn2('hello');
+describe('Mocking tests', () => {
+  // this function must be marked as `async` in order to use `await` within it
+  test('Axios test', async () => {
+    const { getByTestId, findByText } = render(<Result status="Waiting" />);
+    const button = getByTestId('fetch-highscores');
   
-  expect(result).toBe('world');
+    // mock any calls to axios.get with hardcoded return value `data`
+    axios.get.mockResolvedValueOnce({ data });
+  
+    // firing this event causes the axios call to happen
+    fireEvent.click(button);
+  
+    // findBy functions return a promise which we can `await`
+    await findByText('Bob');
+  });
+  
+  test('functions', () => {
+    // we can mock functions using jest.fn()
+    const mockFn = jest.fn();
+    let result = mockFn('hello');
+  
+    // and listen to how many times the fn was called and with what arguments
+    expect(result).toBeUndefined();
+    expect(mockFn).toHaveBeenCalled();
+    expect(mockFn).toHaveBeenCalledWith('hello');
+    expect(mockFn).toHaveBeenCalledTimes(1);
+  
+    // we can also fake return values
+    const mockFn2 = jest.fn(() => 'world');
+    result = mockFn2('hello');
+    
+    expect(result).toBe('world');
+  });
 });
